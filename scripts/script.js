0f8c1f74c8a6038e3129352f2ac839cc854cd0ea
copycorrect <copycorrectkiev@gmail.com>
2021-02-22 15:22:35 +0200

    header+

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,30 @@
-let navMobButton = document.querySelector('.nav-mob-button');
-let navMob = document.querySelector('.nav');
+const d = document;
+
+let navMobButton = d.querySelector('.nav-mob-button');
+let navMob = d.querySelector('.nav');
 
 navMobButton.onclick = () => {
     navMobButton.classList.toggle('open');
     navMob.classList.toggle('open');
+    linkActive();
 }
 
-let nav = document.querySelectorAll('.nav li');
+let nav = d.querySelectorAll('.nav li');
 
-nav.forEach(navLi => {
-    navLi.addEventListener('click', function () {
-        let currents = document.querySelector('.nav').querySelectorAll('.active');
-        currents.forEach(current => current.classList.remove('active'));
-        navLi.querySelector('a').classList.add('active');
-    });
-})
+let linkActive = () =>{
+    nav.forEach(navLi => {
+        navLi.addEventListener('click', function () {
+            let currents = d.querySelector('.nav').querySelectorAll('.active');
+            currents.forEach(current => current.classList.remove('active'));
+            navLi.querySelector('a').classList.add('active');
+            navMobButton.classList.remove('open');
+            navMob.classList.remove('open');
+        });
+    })
+}
+linkActive();
 
-
-let links = document.querySelectorAll('a[href^="#"]');
+let links = d.querySelectorAll('a[href^="#"]');
 links.forEach(link => {
     // console.log(link);
     link.addEventListener('click', function (e) {
@@ -31,14 +38,14 @@ links.forEach(link => {
         }
         window.scrollTo({
             top:
-                document.querySelector('#' + href).offsetTop -
-                document.querySelector('.header').offsetHeight,
+                d.querySelector('#' + href).offsetTop -
+                d.querySelector('.header').offsetHeight,
             behavior: 'smooth',
         });
     });
 });
-let servicesAllButton = document.querySelector('._services-all');
-let servicesBlock = document.querySelector('.services-block');
+let servicesAllButton = d.querySelector('._services-all');
+let servicesBlock = d.querySelector('.services-block');
 
 servicesAllButton.onclick = () => {
     // servicesBlock.classList.add('open');
@@ -46,47 +53,36 @@ servicesAllButton.onclick = () => {
     // servicesAllButton.textContent.toggle('Скрыть дополнительные услуги Palitra Team');
 }
 
-// $(window).scroll(function(){
-//     var $sections = $('section');
-//     $sections.each(function(i,el){
-//         var top  = $(el).offset().top-100;
-//         var bottom = top +$(el).height();
-//         var scroll = $(window).scrollTop();
-//         var id = $(el).attr('id');
-//         if( scroll > top && scroll < bottom){
-//             $('a.active').removeClass('active');
-//             $('a[href="#'+id+'"]').addClass('active');
-//         }
-//     })
-// });
-//
-// let scroll = 0
-//
-// window.addEventListener('scroll', function() {
-//     let blocks = document.querySelectorAll('section');
-//     // console.log(blocks);
-//     blocks.forEach(block => {
-//         scroll = window.scrollY;
-//         console.log('scroll ' + scroll);
-//         if (scroll){
-//             console.log(1);
-//             return 0
-//         }
-//         // console.log('block '+ block);
-//         let top = block.offsetTop;
-//         // console.log('top ' + top);
-//         let bottom = top + block.offsetHeight;
-//         // console.log('bottom ' + bottom);
-//
-//
-//         let id = block.id;
-//         // console.log('id ' + id);
-//         let newLinkActive = `a[href="#${id}"]`;
-//         // console.log('new ' + newLinkActive);
-//         // if( scroll > top && scroll < bottom){
-//         //     links.querySelector('.active').classList.remove('active');
-//         //     links.querySelector('newLinkActive').classList.add('active');
-//         // }
-//     });
-// });
+let blocks = d.querySelectorAll('section');
+let header = d.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+
+    let scroll = window.scrollY;
+
+    for (let i = 0; i < blocks.length; i++) {
+
+        let block = blocks[i];
+        let top = block.offsetTop - header.offsetHeight;
+        let bottom = block.offsetTop + block.offsetHeight;
+
+        if (scroll > top && scroll < bottom) {
+
+            let isDark = block.classList.contains('bg-dark');
+
+            if (!isDark){
+                header.style.backgroundColor = 'gray';
+            } else {
+                header.style.backgroundColor = 'black';
+            }
+
+            // console.log(isDark);
+
+
+            // console.log(newLinkActive);
+            // links.querySelector('.active').classList.remove('active');
+            // links.querySelector('newLinkActive').classList.add('active');
+        }
+    }
+});
 
